b15e954b0523dd9adeb00d780da56f80ebcbf6ae
JoeWroe <joe_wroe@icloud.com>
2016-01-27 11:08:53 +0000

    Challenge 5 - Add the balance
    In order to use public transport
    As a customer
    I want money on my card

--- spec/feature/user_stories_spec.rb
+++ spec/feature/user_stories_spec.rb
@@ -1,3 +1,14 @@
 describe 'user stories' do
-  
+  # In order to use public transport
+  # As a customer
+  # I want money on my card
+  it 'so that a user can have money on a card, card needs to store money' do
+    card = Oystercard.new
+    expect{ card.balance }.not_to raise_error
+  end
+
+  it 'should automatically start with a balance of 0' do
+    card =Oystercard.new
+    expect(card.balance).to eq 0
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,18 @@
 require 'oystercard.rb'
 
 describe Oystercard do
+  subject(:card) { described_class.new }
+
+  describe ' #balance' do
+
+    it { is_expected.to respond_to(:balance) }
+
+    it 'has a balance of 0 if never topped up' do
+      expect(card.balance).to eq 0
+    end
+
+  end
+
+
+
 end
